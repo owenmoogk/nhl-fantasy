e3d55381acb4a45d89293ab1aa9d609959749fea
owenmoogk <owenmoogk@gmail.com>
2021-03-25 10:42:37 -0400

    teams is now working.... needs graphics

--- src/teams/helpers.py
+++ src/teams/helpers.py
@@ -1,22 +1,39 @@
 import requests
+from .models import Team
+import time
 
 def getTeams(request):
-    baseUrl = "https://statsapi.web.nhl.com/"
-    r = requests.get(baseUrl + '/api/v1/teams')
-    originalData = r.json()
-    originalData = originalData["teams"]
-    for team in originalData:
-        id = team["id"]
-        try:
-            databaseTeam = Team.objects.get(id = id)
-        except Exception:
-            Team.objects.create(id = id)
-            databaseTeam = Team.objects.get(id = id)
-        databaseTeam.name = team["name"]
-        databaseTeam.abbreviation = team["abbreviation"]
-        databaseTeam.link = team["link"]
-        x = databaseTeam.users
-        x.append(request.user.id)
-        databaseTeam.users = x
-        databaseTeam.save()
-    print("done dude")
+    # trys, except the file doesnt exist
+    try:
+        with open("vars.txt", "r") as varsText:
+            date = varsText.readline(0)
+            date = int(date)
+    except Exception:
+        # create it, it is now 1970 /shrug
+        with open("vars.txt", "w"):
+            date = 0
+
+    # ONLY RUNS THIS EVERY DAYISH
+    if time.time()-86400 > date:
+
+        # update the date file
+        with open(".vars.txt", "w") as varsText:
+            varsText.write(str(time.time()))
+
+        baseUrl = "https://statsapi.web.nhl.com/"
+        r = requests.get(baseUrl + '/api/v1/teams')
+        originalData = r.json()
+        originalData = originalData["teams"]
+        for team in originalData:
+            id = team["id"]
+            try:
+                databaseTeam = Team.objects.get(id = id)
+            except Exception:
+                Team.objects.create(id = id)
+                databaseTeam = Team.objects.get(id = id)
+            databaseTeam.name = team["name"]
+            databaseTeam.abbreviation = team["abbreviation"]
+            databaseTeam.link = team["link"]
+            databaseTeam.save()
+
+        print("done")
--- src/teams/views.py
+++ src/teams/views.py
@@ -3,13 +3,73 @@ from django.contrib.auth.decorators import login_required
 from .models import Team
 from threading import *
 from .helpers import *
+from django.http import HttpResponseRedirect
 
 @login_required
 def myTeamView(request):
+
+    if request.method == "POST":
+        if "unstar" in request.POST:
+            obj = Team.objects.get(id = request.POST.get("id"))
+            userList = obj.users
+            userList.remove(request.user.id)
+            obj.users = userList
+            obj.save()
+            
+        # redirects here in a get request... fixes refresh problem
+        return HttpResponseRedirect('/myTeams')
+
+    # pull data if needed
     t1 = Thread(target=getTeams, args=(request, ))
     t1.start()
-    return(render(request, "teams/myTeams.html", {}))
+
+    # main view
+    teams = list(Team.objects.all())
+    data = []
+    # appending the teams that the user has saved
+    if teams != None:
+        for team in teams:
+            if team.users:
+                if request.user.id in team.users:
+                    data.append(team)
+    context = {
+        "teams": data,
+    }
+    return(render(request, "teams/myTeams.html", context))
+
 
 @login_required
 def allTeamView(request):
-    return(render(request, "teams/allTeams.html", {}))
+
+    # pull data if needed
+    t1 = Thread(target=getTeams, args=(request, ))
+    t1.start()
+
+    # if the user wants to save a team
+    if request.method == "POST":
+        if "star" in request.POST:
+            obj = Team.objects.get(id = request.POST.get("id"))
+            userList = obj.users
+            if userList == None:
+                userList = []
+            if request.user.id not in userList:
+                userList.append(request.user.id)
+            obj.users = userList
+            obj.save()
+        
+        if "unstar" in request.POST:
+            obj = Team.objects.get(id = request.POST.get("id"))
+            userList = obj.users
+            userList.remove(request.user.id)
+            obj.users = userList
+            obj.save()
+
+        # redirects here in a get request... fixes refresh problem
+        return HttpResponseRedirect('/allTeams')
+
+    # main view
+    data = Team.objects.all()
+    context = {
+        "teams": data,
+    }
+    return(render(request, "teams/allTeams.html", context))
