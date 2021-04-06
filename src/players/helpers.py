1cf9237b0f3b612d4532c1386713aa49a53b1e52
owenmoogk <owenmoogk@gmail.com>
2021-04-06 10:41:43 -0400

    updated goalie and player models to work, cleaned up helper code to implement dictionary models

--- src/players/helpers.py
+++ src/players/helpers.py
@@ -2,8 +2,6 @@ from teams.models import Team
 import time, requests
 from .models import *
 
-baseUrl = "https://statsapi.web.nhl.com/"
-
 def updatePlayers():
 
     # trys, except the file doesnt exist
@@ -17,86 +15,80 @@ def updatePlayers():
             date = 0
 
     # ONLY RUNS THIS EVERY DAYISH
-    if time.time()-86400 > date:
-        print("updating players")
+    if time.time()-86400 > date: #fix
+
+        print("getting the player data")
+
+        # DOCUMENTATION
+        # 1. Gets the ids for all active players, team by team
+        # 2. For each player, gets the metadata and their stats, join together into single dictionary
+        # 3. Put final dictionary into a model and save it
+
         # update the date file
         with open("playerTime.txt", "w") as varsText:
             varsText.write(str(time.time()))
 
+        playerIds = []
         teams = Team.objects.all()
-        # the players will be stored in list of dictionarys, storing key value pairs which will later be put into the model
-        playerDicts = []
+
         for team in teams:
             teamId = team.id
-            playerDicts += getTeamRosterIds(teamId)
+            playerIds += getTeamRosterIds(teamId)
+            # just so i dont get screwed for making too many requests
             time.sleep(0.1)
 
-        for player in playerDicts:
-            # player is the dictionary
-            stats = getPlayerStatsById(player["id"])
+        for playerId in playerIds:
 
-            if stats == None:
-                continue
+            playerStats = getPlayerStats(playerId)
+            playerMetadata = getPlayerMetadata(playerId)
+            playerData =  playerMetadata | playerStats
 
-            try:
-                player['assists'] = stats['assists']
-                player['goals'] = stats['goals']
-                player['pim'] = stats['pim']
-                player['shots'] = stats['shots']
-                player['points'] = stats['points']
+            print(playerData)
 
-                try:
-                    currPlayer = Player.objects.create(**player)
-                except:
-                    currPlayer = Player.objects.get(id = player["id"])
-                    currPlayer = Player(**player)
-                finally:
-                    currPlayer.save()
+            if playerData["primaryPosition"]["code"] != "G":
+                instance, created = Player.objects.get_or_create(id=playerId)
+                for attr, value in playerData.items(): 
+                    setattr(instance, attr, value)
+                instance.save()
 
-            # player is a goalie
-            except Exception:
-                try:
-                    player["wins"] = stats["wins"]
-                    player["losses"] = stats["losses"]
-                    player['savePercentage'] = stats['savePercentage']
+            else:
+                instance, created = Goalie.objects.get_or_create(id=playerId)
+                for attr, value in playerData.items(): 
+                    setattr(instance, attr, value)
+                instance.save()
 
-                    try:
-                        currPlayer = Goalie.objects.create(**player)
-                    except:
-                        currPlayer = Goalie.objects.get(id = player["id"])
-                        currPlayer = Goalie(**player)
-                    finally:
-                        currPlayer.save()
-
-                except Exception as e:
-                    print("had an error getting the data into the database")
-                    print(e)
-                    print(player["id"])
-                    print(stats)
-
-
-# this might be a problem, i might have to add in some delay to not get an error for accessing too fast
-def getPlayerStatsById(id):
+# returning the players stats by id. If problem returns none
+def getPlayerStats(id):
+    baseUrl = "https://statsapi.web.nhl.com/"
     r = requests.get(baseUrl + "/api/v1/people/" + str(id) + "/stats?stats=statsSingleSeason&season=20202021")
     stats = r.json()
     try:
         stats = stats["stats"][0]["splits"][0]["stat"]
     except:
-        stats = None
+        stats = {}
     return(stats)
 
+def getPlayerMetadata(id):
+    baseUrl = "https://statsapi.web.nhl.com/"
+    r = requests.get(baseUrl + "/api/v1/people/" + str(id))
+    stats = r.json()
+    try:
+        stats = stats["people"][0]
+        # just for ease of storage in django
+        stats["number"] = stats["primaryNumber"]
+        stats["teamId"] = stats["currentTeam"]["id"]
+    except:
+        stats = {}
+    return(stats)
+
+# getting the ids of all the players on a given team
+# helper function
 def getTeamRosterIds(teamId):
+    baseUrl = "https://statsapi.web.nhl.com/"
     r = requests.get(baseUrl + "api/v1/teams/" + str(teamId) + "/roster")
     roster = r.json()
-    # i dont need the copyright, only care about the roster
     roster = roster["roster"]
-    # getting rid of the person label
-    for index, player in enumerate(roster):
-        tmp = {
-            "id": player["person"]["id"],
-            "name": player["person"]["fullName"],
-            "number": player["jerseyNumber"],
-            "teamId": teamId,
-        }
-        roster[index] = tmp
-    return(roster)
+    ids = []
+    for player in roster:
+        ids.append(player["person"]["id"])
+    return(ids)
--- src/players/models.py
+++ src/players/models.py
@@ -2,23 +2,117 @@ from django.db import models
 
 # Create your models here.
 class Player(models.Model):
+
+    # METADATA
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=120)
+    firstName = models.CharField(max_length=120)
+    lastName = models.CharField(max_length=120)
     number = models.IntegerField(default=0)
+
+    birthDate = models.CharField(max_length=120)
+    currentAge = models.IntegerField(default=0)
+    birthCity = models.CharField(max_length=120)
+    birthCountry = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+
+    height = models.CharField(max_length=100)
+    weight = models.IntegerField(default=0)
+
+    alternateCaptain = models.BooleanField(default=False)
+    captian = models.BooleanField(default=False)
+
+    shootsCatches = models.CharField(max_length=1)
+
+    currentTeam = models.JSONField(null=True)
     teamId = models.IntegerField(default=0)
+    primaryPosition = models.JSONField(null=True)
+
+
+    # STATS
+    timeOnIce = models.CharField(max_length=120)
+
     assists = models.IntegerField(default=0)
     goals = models.IntegerField(default=0)
-    points = models.IntegerField(default=0)
     pim = models.IntegerField(default=0)
     shots = models.IntegerField(default=0)
+    games = models.IntegerField(default=0)
+    hits = models.IntegerField(default=0)
+
+    powerPlayGoals = models.IntegerField(default=0)
+    powerPlayPoints = models.IntegerField(default=0)
+    powerPlayTimeOnIce = models.CharField(max_length=120)
+
+    shortHandedGoals = models.IntegerField(default=0)
+    shortHandedPoints = models.IntegerField(default=0)
+    shortHandedTimeOnIce = models.CharField(max_length=120)
+
+
+    faceOffPct = models.DecimalField(decimal_places=1, max_digits=100, default=0)
+    shotPct = models.DecimalField(decimal_places=1, max_digits=100, default=0)
+
+    plusMinus = models.IntegerField(default=0)
+    points = models.IntegerField(default=0)
+
+    shifts = models.IntegerField(default=0)
+
     users = models.JSONField(null=True)
 
 class Goalie(models.Model):
+
+    # METADATA
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=120)
+    firstName = models.CharField(max_length=120, default='')
+    lastName = models.CharField(max_length=120, default='')
     number = models.IntegerField(default=0)
+
+    birthDate = models.CharField(max_length=120, default='')
+    currentAge = models.IntegerField(default=0)
+    birthCity = models.CharField(max_length=120, default='')
+    birthCountry = models.CharField(max_length=100, default='')
+    nationality = models.CharField(max_length=100, default='')
+
+    height = models.CharField(max_length=100, default='')
+    weight = models.IntegerField(default=0)
+
+    alternateCaptain = models.BooleanField(default=False)
+    captian = models.BooleanField(default=False)
+
+    shootsCatches = models.CharField(max_length=1, default='')
+
+    currentTeam = models.JSONField(null=True)
     teamId = models.IntegerField(default=0)
+    primaryPosition = models.JSONField(null=True)
+
+
+    # STATS
+    timeOnIce = models.CharField(max_length=120, default='')
+    timeOnIcePerGame = models.CharField(max_length=120, default='')
+
+    ot = models.IntegerField(default=0)
+    shutouts = models.IntegerField(default=0)
+    ties = models.IntegerField(default=0)
     wins = models.IntegerField(default=0)
     losses = models.IntegerField(default=0)
-    savePercentage = models.DecimalField(decimal_places=3, max_digits=1000)
+
+    saves = models.IntegerField(default=0)
+    powerPlaySaves = models.IntegerField(default=0)
+    shortHandedSaves = models.IntegerField(default=0)
+    evenSaves = models.IntegerField(default=0)
+
+    shotsAgainst = models.IntegerField(default=0)    
+    shortHandedShots = models.IntegerField(default=0)
+    evenShots = models.IntegerField(default=0)
+    powerPlayShots = models.IntegerField(default=0)
+
+    savePercentage = models.DecimalField(decimal_places=14, max_digits=100, default=0)
+    powerPlaySavePercentage = models.DecimalField(decimal_places=14, max_digits=100, default=0)
+    shortHandedSavePercentage = models.DecimalField(decimal_places=14, max_digits=100, default=0)
+    evenStrengthSavePercentage = models.DecimalField(decimal_places=14, max_digits=100, default=0)
+
+    goalsAgainst = models.IntegerField(default=0)
+    goalAgainstAverage = models.DecimalField(decimal_places=1, max_digits=100, default=0)
+
+    games = models.IntegerField(default=0)
+    gamesStarted = models.IntegerField(default=0) 
+
     users = models.JSONField(null=True)
